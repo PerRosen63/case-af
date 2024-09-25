5c03de68946b7b9f3cfdac3547666190340d227b
Carinalak <carina.lakosil@gmail.com>
2024-09-25 10:52:25 +0200

    :construction: Added models and service

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import { router } from "./Router";
 
 function App() {
   return <><h1>Test</h1></>;
+  <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
