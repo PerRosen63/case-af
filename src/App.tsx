2671ccc5dd1e738291ad2da43d5aec44a7b34f63
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-09-30 10:47:02 +0200

    🚧 Commented out code causing infinite loop

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@
 // import { JobContext } from "./contexts/JobContext";
 // import { ActionType, JobReducer } from "./reducers/jobReducer";
 // import { getJobs } from "./service/jobService";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Content from "./components/Content";
-import "./App.css";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Content from "./components/Content";
+import { RouterProvider } from "react-router-dom";
+// import "./App.css";
+import { router } from "./Router";
 
 function App() {
   /* const [jobs, dispatch] = useReducer(JobReducer, []);
@@ -24,17 +26,17 @@ function App() {
     getData();
   }, [jobs]); */
 
-  function myFunction(e: any) {
+  /* function myFunction(e: any) {
     console.log(e);
-  }
+  } */
 
   return (
     <>
       {/* <JobContext.Provider value={{ jobs, dispatch }}> */}
-      {/* <RouterProvider router={router}></RouterProvider> */}
-      <Header />
+      <RouterProvider router={router}></RouterProvider>
+      {/* <Header />
       <Content onButtonClick={myFunction} /> {}
-      <Footer />
+      <Footer /> */}
       {/* </JobContext.Provider> */}
     </>
   );
