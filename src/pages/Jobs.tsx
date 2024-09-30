4c06f50f460018c052976e8922ae49dfc345ea4c
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-09-30 15:20:07 +0200

    ✨ Uncommented OccupationList

--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -1,19 +1,19 @@
 import { useContext } from "react";
 import { JobsPresentation } from "../components/JobsPresentation";
 import { JobContext } from "../contexts/JobContext";
-//import { OccupationsList } from "../components/OccupationsList";
+import { OccupationsList } from "../components/OccupationsList";
 
 export const Jobs = () => {
   const { jobs } = useContext(JobContext);
   return (
     <div>
       <div>
-      <h1>Jobs</h1>
-      <p>Antal jobb: {jobs.length}</p> 
-      <JobsPresentation />
-    </div>
+        <h1>Jobs</h1>
+        <p>Antal jobb: {jobs.length}</p>
+        <JobsPresentation />
+      </div>
 
-      {/*<OccupationsList></OccupationsList>*/}
+      <OccupationsList></OccupationsList>
       {/*<JobsPresentation></JobsPresentation>*/}
     </div>
   );
