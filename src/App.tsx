20cdd853d332cfdb0ae6eaf60c2b20e6ace5767a
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-09-30 15:13:55 +0200

    🐛 Added dependendy array

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
-import { JobContext } from "./contexts/JobContext";
 import { router } from "./Router";
+import { JobContext } from "./contexts/JobContext";
 import { useReducer, useEffect } from "react";
 import { JobReducer, ActionType } from "./reducers/JobReducer";
 import { getJobs } from "./service/jobService";
 
 function App() {
-   const [jobs, dispatch] = useReducer(JobReducer, []);
+  const [jobs, dispatch] = useReducer(JobReducer, []);
 
-   useEffect(() => {
+  useEffect(() => {
     const getData = async () => {
       const data = await getJobs();
       dispatch({
@@ -17,22 +17,15 @@ function App() {
         payload: JSON.stringify(data),
       });
     };
-    if (jobs.length > 0 ) return; 
+    if (jobs.length > 0) return;
     getData();
-  }); 
-/*
-  function myFunction(e: string) {
-    console.log(e);
-  }
-*/
+  }, [jobs.length]);
+
   return (
     <>
-       <JobContext.Provider value={{ jobs, dispatch }}>
-      <RouterProvider router={router}>
-      
-      {/*<Content onButtonClick={myFunction} /> {}*/}
-      </RouterProvider> 
-      </JobContext.Provider> 
+      <JobContext.Provider value={{ jobs, dispatch }}>
+        <RouterProvider router={router}></RouterProvider>
+      </JobContext.Provider>
     </>
   );
 }
