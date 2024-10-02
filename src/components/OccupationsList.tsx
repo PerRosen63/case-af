08004e8328a4dde7d28d0afc76784933580d6758
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-10-02 12:55:30 +0200

    🔥 deleted old code

--- src/components/OccupationsList.tsx
+++ src/components/OccupationsList.tsx
@@ -5,13 +5,10 @@ import {
   DigiFormCheckbox,
   DigiFormFilter,
 } from "@digi/arbetsformedlingen-react";
-// import { DigiIconChevronRight } from "@digi/arbetsformedlingen-react";
 
 export const OccupationsList = () => {
   console.log("test:", "test"); // Infinite loop?
   const [occupationsGroup, setOccupationsGruop] = useState<IOccupation[]>([]);
-
-  // const [openGroups, setOpenGroups] = useState<{ [id: string]: boolean }>({});
 
   // State to track filter selections for each group
   const [selectedNarrower, setSelectedNarrower] = useState<{
@@ -36,21 +33,6 @@ export const OccupationsList = () => {
     }));
   };
 
-  /* const toggleGroup = (groupId: string) => {
-    setOpenGroups((prevOpenGroups) => {
-      const updatedOpenGroups = occupationsGroup.reduce((acc, group) => {
-        acc[group.id] = false;
-        return acc;
-      }, {} as { [id: string]: boolean });
-
-      // Open only the clicked group
-      updatedOpenGroups[groupId] = !prevOpenGroups[groupId];
-      console.log("openGroups after update:", updatedOpenGroups); // Check if the state reflects the change
-      return updatedOpenGroups;
-    });
-    console.log("test2:", "test2");
-  }; */
-
   return (
     <>
       <h1>Occupations List</h1>
@@ -58,34 +40,6 @@ export const OccupationsList = () => {
         <ul className="occupation-group">
           {occupationsGroup.map((occupationGroup) => (
             <li className="occupation-group-item" key={occupationGroup.id}>
-              {/* <input
-                type="checkbox"
-                name={occupationGroup.preferred_label}
-                id={occupationGroup.id}
-              /> */}
-
-              {/*    <button onClick={() => toggleGroup(occupationGroup.id)}>
-                <span className="btn-content">
-                  <p>{occupationGroup.preferred_label}</p>
-                  <DigiIconChevronRight></DigiIconChevronRight>
-                </span>
-              </button>
-              <ul
-                className={`occupation ${
-                  openGroups[occupationGroup.id] ? "open" : ""
-                }`}
-              >
-                {occupationGroup.narrower.map((narrowerOccupation) => (
-                  <li key={narrowerOccupation.id}>
-                    <input
-                      type="checkbox"
-                      name={narrowerOccupation.preferred_label}
-                      id={narrowerOccupation.id}
-                    />
-                    {narrowerOccupation.preferred_label}
-                  </li>
-                ))}
-              </ul> */}
               <DigiFormCheckbox afLabel="Välj alla"></DigiFormCheckbox>
               <DigiFormFilter
                 afFilterButtonText={occupationGroup.preferred_label}
