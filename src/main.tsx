0d0d7fb9bce14c2e155ee89b89432e20becba72b
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-10-02 12:44:26 +0200

    🐛 removed faulty link

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-//import '@digi/arbetsformedlingen/dist/digi-arbetsformedlingen/digi-arbetsformedlingen.css';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
