05be1969fbce2d4322af5804d2af8882f46997b8
Marcusey <146931671+Marcusey@users.noreply.github.com>
2024-10-04 00:07:00 +0200

    :construction: Main fillings

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,26 @@
-
 export const Home = () => {
+  return (
+    <main>
+      <section className="grid-item">
+        <h1>Välkommen till vår Jobbplattform</h1>
+        <p>Här kan du hitta och söka efter de senaste jobben inom olika branscher.</p>
+      </section>
 
-  return (<>
-  <h1>Home</h1>
+      <section className="grid-item">
+        <h2>Sök jobb</h2>
+        <p>
+          Upptäck olika jobbannonser med vår kraftfulla sökfunktion och hitta ditt drömjobb
+          snabbt och enkelt.
+        </p>
+      </section>
 
-    
-
-  </>)
-  
-  }
+      <section className="grid-item">
+        <h2>Skapa profil</h2>
+        <p>
+          Skapa en profil och låt arbetsgivare hitta dig genom vår plattform. Det har aldrig varit
+          enklare att bli synlig för företag.
+        </p>
+      </section>
+    </main>
+  );
+};
