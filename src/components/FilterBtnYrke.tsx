92f4f2c1d2b9f8ccf0dc0d90795520770edcf330
PerRosen63 <146942472+PerRosen63@users.noreply.github.com>
2024-10-07 19:23:03 +0200

    🔥 Moved code to components

--- src/components/FilterBtnYrke.tsx
+++ src/components/FilterBtnYrke.tsx
@@ -1,7 +1,12 @@
 import {
+  DigiButton,
+  DigiExpandableAccordion,
   DigiFormCheckbox,
   DigiFormFilter,
+  DigiIconChevronDown,
 } from "@digi/arbetsformedlingen-react";
+import { OccupationsList } from "./OccupationsList";
+import { ButtonSize, ButtonVariation } from "@digi/arbetsformedlingen";
 
 /*
 interface FilterBtnYrkeProps {
@@ -9,106 +14,20 @@ interface FilterBtnYrkeProps {
   setSelectedOccupations: (occupations: string[]) => void;
 }
 */
+
 export const FilterBtnYrke = () => {
-  /*
-export const FilterBtnYrke = ({ selectedOccupations, setSelectedOccupations }: FilterBtnYrkeProps) => {
-  const [occupationItems, setOccupationItems] = useState<{ label: string; value: string }[]>([]);
-*/
-  // Hämta yrken vid komponentens montering
-  /*
-  useEffect(() => {
-    const fetchOccupations = async () => {
-      const occupations = await getOccupation();
-      const formattedOccupations = occupations.map((occupation) => ({
-        label: occupation.preferred_label,
-        value: occupation.id,
-      }));
-      setOccupationItems(formattedOccupations);
-    };
-
-    fetchOccupations();
-  }, []);
-
-  const handleOccupationChange = (selectedValues: string[]) => {
-    setSelectedOccupations(selectedValues);
-  };
-  */
   return (
     <>
-      <div className="filter-buttons">
-        <DigiFormFilter
-          afFilterButtonText="Ort"
-          afSubmitButtonText="Filtrera"
-          afListItems={[
-            { id: "omr1", label: "Område 1" },
-            { id: "omr2", label: "Område 2" },
-            { id: "omr3", label: "Område 3" },
-          ]}
-          afCheckItems={["omr2"]} // optional, override internal check state of component with filter ids
-          onAfChangeFilter={(e) => console.log(e.detail.id, e.detail.isChecked)}
-          onAfResetFilter={() => console.log("reset filter")}
-          onAfSubmitFilter={(e) =>
-            console.log("submit filter", e.detail.listItems, e.detail.checked)
-          }
-          onAfCloseFilter={(e) =>
-            console.log("submit filter", e.detail.listItems, e.detail.checked)
-          }
-        ></DigiFormFilter>
-
-        <DigiFormFilter
-          afFilterButtonText="Yrke"
-          afSubmitButtonText="Filtrera"
-          afListItems={[
-            { id: "omr1", label: "Område 1" },
-            { id: "omr2", label: "Område 2" },
-            { id: "omr3", label: "Område 3" },
-          ]}
-          afCheckItems={["omr2"]} // optional, override internal check state of component with filter ids
-          onAfChangeFilter={(e) => console.log(e.detail.id, e.detail.isChecked)}
-          onAfResetFilter={() => console.log("reset filter")}
-          onAfSubmitFilter={(e) =>
-            console.log("submit filter", e.detail.listItems, e.detail.checked)
-          }
-          onAfCloseFilter={(e) =>
-            console.log("submit filter", e.detail.listItems, e.detail.checked)
-          }
-        ></DigiFormFilter>
-
-        <DigiFormFilter
-          afFilterButtonText="Filter"
-          afSubmitButtonText="Filtrera"
-          afListItems={[
-            { id: "omr1", label: "Område 1" },
-            { id: "omr2", label: "Område 2" },
-            { id: "omr3", label: "Område 3" },
-          ]}
-          afCheckItems={["omr2"]} // optional, override internal check state of component with filter ids
-          onAfChangeFilter={(e) => console.log(e.detail.id, e.detail.isChecked)}
-          onAfResetFilter={() => console.log("reset filter")}
-          onAfSubmitFilter={(e) =>
-            console.log("submit filter", e.detail.listItems, e.detail.checked)
-          }
-          onAfCloseFilter={(e) =>
-            console.log("submit filter", e.detail.listItems, e.detail.checked)
-          }
-        ></DigiFormFilter>
-
-        <DigiFormCheckbox afLabel="Kryssruta" afChecked />
+      <div className="button-row">
+        <DigiButton
+          afSize={ButtonSize.MEDIUM}
+          afVariation={ButtonVariation.PRIMARY}
+          afFullWidth={false}
+        >
+          Yrke <DigiIconChevronDown />
+        </DigiButton>
       </div>
-
-      {/*
-      <DigiFormSelectFilter
-        afFilterButtonTextLabel="Välj yrke"
-        afDescription="Välj ett eller flera yrken från listan"
-        afFilterButtonText="Välj yrke"
-        afSubmitButtonText="Filtrera"
-        afMultipleItems={true}
-        afValidation={FormSelectFilterValidation.NEUTRAL}
-        afListItems={occupationItems}
-        afOnChange={handleOccupationChange}
-        afSelectedItems={selectedOccupations}
-      />
-      */}
+      <OccupationsList></OccupationsList>
     </>
   );
 };
--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -1,29 +1,20 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { FilterBtnYrke } from "../components/FilterBtnYrke";
 import { JobsPresentation } from "../components/JobsPresentation";
 import { JobContext } from "../contexts/JobContext";
-import { OccupationsList } from "../components/OccupationsList";
 import { SearchJob } from "../components/SearchJob";
 
 export const Jobs = () => {
   const { jobs } = useContext(JobContext);
 
-  const [selectedOccupations, setSelectedOccupations] = useState<string[]>([]);
-
   return (
     <div>
       <div>
         <SearchJob />
-        <FilterBtnYrke
-          selectedOccupations={selectedOccupations}
-          setSelectedOccupations={setSelectedOccupations}
-        />
-        {/*         <OccupationsList selectedOccupations={selectedOccupations} />
-         */}{" "}
+        <FilterBtnYrke />
         <h1>Jobs</h1>
         <p>Antal jobb: {jobs.length}</p>
         <JobsPresentation />
-        <OccupationsList></OccupationsList>
       </div>
     </div>
   );
