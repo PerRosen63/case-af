2c9a8025278a297ddc80b68f1a958f91f24aee7f
Marcusey <146931671+Marcusey@users.noreply.github.com>
2024-10-08 10:50:54 +0200

    :art: added jpg

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,56 +1,19 @@
 import { ButtonVariation, TypographyVariation } from "@digi/arbetsformedlingen";
 import { DigiButton, DigiLayoutContainer, DigiTypography } from "@digi/arbetsformedlingen-react";
 import { Link } from "react-router-dom";
+import fasadImage from '../assets/images/husfasad.jpg';
+
 
 export const Home = () => {
   return (
     <DigiTypography afVariation={TypographyVariation.SMALL}>
-      <main>
-        <DigiLayoutContainer afVerticalPadding>
-          <h2>Välkommen till vår Jobbplattform</h2>
-          <p>
-            Här kan du hitta och söka efter de senaste jobben inom olika branscher.
-          </p>
-
-          <Link to="/job/1">
-          <DigiButton afVariation={ButtonVariation.PRIMARY}>
-              Hitta jobb
-            </DigiButton>
-          </Link>
-
-        </DigiLayoutContainer>
-
-        <DigiLayoutContainer afVerticalPadding>
-          <h2>Sök jobb</h2>
-          <p>
-            Upptäck olika jobbannonser med vår kraftfulla sökfunktion och hitta ditt drömjobb
-            snabbt och enkelt.
-          </p>
-        </DigiLayoutContainer>
-
-        <DigiLayoutContainer afVerticalPadding>
-          <h2>Skapa profil</h2>
-          <p>
-            Skapa en profil och låt arbetsgivare hitta dig genom vår plattform. Det har aldrig varit
-            enklare att bli synlig för företag.
-          </p>
-        </DigiLayoutContainer>
-      </main>
-    </DigiTypography>
-  );
-};
-
-
-/* import { DigiLayoutContainer } from "@digi/arbetsformedlingen-react";
-
-export const Home = () => {
-  return (
     <main>
       <DigiLayoutContainer afVerticalPadding>
         <h2>Välkommen till vår Jobbplattform</h2>
         <p>
           Här kan du hitta och söka efter de senaste jobben inom olika branscher.
         </p>
+         <img src={fasadImage} alt="foretagsnamn-pa-husfasad" style={{ width: "60%", height: "auto" }} />
       </DigiLayoutContainer>
 
       <DigiLayoutContainer afVerticalPadding>
@@ -59,10 +22,17 @@ export const Home = () => {
           Upptäck olika jobbannonser med vår kraftfulla sökfunktion och hitta ditt drömjobb
           snabbt och enkelt.
         </p>
+
+        <Link to="/job/1">
+          <DigiButton afVariation={ButtonVariation.PRIMARY}>
+              Hitta jobb
+            </DigiButton>
+          </Link>
+
       </DigiLayoutContainer>
 
       <DigiLayoutContainer afVerticalPadding>
-        <h2>Skapa profil</h2>
+        <h3>Skapa profil</h3>
         <p>
           Skapa en profil och låt arbetsgivare hitta dig genom vår plattform. Det har aldrig varit
           enklare att bli synlig för företag.
@@ -70,46 +40,26 @@ export const Home = () => {
       </DigiLayoutContainer>
 
       <DigiLayoutContainer afVerticalPadding>
-        <h3 id="arbetlos">Arbetslös, vad händer nu?</h3>
+        <h3>Arbetslös, vad händer nu?</h3>
         <p>
           Inskrivning, planeringssamtal, a-kassa, Det här behöver du veta.
         </p>
       </DigiLayoutContainer>
 
       <DigiLayoutContainer afVerticalPadding>
-        <h3 id="extra-stod">Extra stöd</h3>
-        <p>
-          Arbetsmarknadsutbildning, praktik, stöd vid funktionsnedsättning, A-Ö.
-        </p>
-      </DigiLayoutContainer>
-
-      <DigiLayoutContainer afVerticalPadding>
-        <h3 id="hitta-jobb">Så hittar du jobbet</h3>
+        <h3>Så hittar du jobbet</h3>
         <p>
           Platsbanken, skapa profil, starta eget. Flera sätt att hitta rätt jobb.
         </p>
       </DigiLayoutContainer>
 
       <DigiLayoutContainer afVerticalPadding>
-        <h3 id="cv-ansokan">CV, ansökan och intervju</h3>
-        <p>
-          Tips när du söker jobb. Validera dina betyg och intyg.
-        </p>
-      </DigiLayoutContainer>
-
-      <DigiLayoutContainer afVerticalPadding>
-        <h3 id="yrken-framtid">Yrken och framtid</h3>
-        <p>
-          Yrken, framtidsutsikter, utbildning, intresseguide – gör smarta yrkesval.
-        </p>
-      </DigiLayoutContainer>
-
-      <DigiLayoutContainer afVerticalPadding>
-        <h3 id="utbildning-studier">Utbildning och studier</h3>
+        <h3>Utbildning och studier</h3>
         <p>
           Utbilda dig till jobb. Studiestöd och utbildningsvägar.
         </p>
       </DigiLayoutContainer>
     </main>
+        </DigiTypography>
   );
-}; */
+};
